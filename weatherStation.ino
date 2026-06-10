65d0c68acda1212a02c3fab549957b9724eb834e
LeslyDTech <236714628+LeslyDTech@users.noreply.github.com>
2026-06-10 13:57:48 -0400

    fixed code and pins

--- weatherStation.ino
+++ weatherStation.ino
@@ -1,20 +1,18 @@
-#include <DHT.h>
-#include <DHT_U.h>
-
 #include <LiquidCrystal.h>
 #include <DHT.h>
 
-// LCD pins: RS, E, D4, D5, D6, D7
-LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
+LiquidCrystal lcd(11, 12, 2, 3, 4, 5);
 
-#define DHTPIN 8
+#define DHTPIN 7
 #define DHTTYPE DHT11
 
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
+  pinMode(DHTPIN, INPUT_PULLUP);
   lcd.begin(16, 2);
   dht.begin();
+  delay(3000);
   lcd.print("Weather Station");
   delay(2000);
   lcd.clear();
@@ -27,22 +25,20 @@ void loop() {
   if (isnan(humidity) || isnan(tempC)) {
     lcd.setCursor(0, 0);
     lcd.print("Sensor Error!   ");
-    delay(2000);
+    delay(2500);
     return;
   }
 
-  // Row 1: Temperature
   lcd.setCursor(0, 0);
   lcd.print("Temp: ");
   lcd.print(tempC, 1);
-  lcd.print((char)223); // degree symbol
+  lcd.print((char)223);
   lcd.print("C  ");
 
-  // Row 2: Humidity
   lcd.setCursor(0, 1);
   lcd.print("Humidity: ");
   lcd.print(humidity, 1);
   lcd.print("%  ");
 
-  delay(2000);
+  delay(2500);
 }
